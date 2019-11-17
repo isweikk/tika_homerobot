e9f488f2e82669342823a3731c4dfe0092d1ab74
weikk <weikk90@163.com>
2019-11-18 02:35:18 +0800

    galaxy design

--- src/disp/inc/disp_main_ui.h
+++ src/disp/inc/disp_main_ui.h
@@ -2,8 +2,15 @@
 #define _DISP_MAIN_UI_H_
 
 #include "os_type.h"
+#include "disp_texture.h"
 
+class DispMainUi{
+public:
 
+private:
+    class Universe;
+    Universe *universe_;
+};
 
 #endif
 
--- src/disp/src/disp_main_ui.cpp
+++ src/disp/src/disp_main_ui.cpp
@@ -33,7 +33,7 @@ static Bool dbufp;
 #define DEF_DBUF   "True"
 
 #define LRAND()         ((long) (random() & 0x7fffffff))
-#define FLOATRAND ((double) LRAND() / ((double) MAXRAND))
+#define FLOAT_RAND_NUM ((double) LRAND() / ((double) MAXRAND))
 
 #if 0
 #define WRAP       1  /* Warp around edges */
@@ -60,14 +60,16 @@ static Bool dbufp;
 #define QCONS    0.001
 
 
-#define COLORBASE  16
+#define COLOR_BASE  16
 /* colors per galaxy */
-/* #define COLORSTEP  (NUMCOLORS/COLORBASE) */
-# define COLORSTEP (MI_NCOLORS(mi)/COLORBASE)
+/* #define COLORSTEP  (NUMCOLORS/COLOR_BASE) */
+# define COLORSTEP (MI_NCOLORS(mi)/COLOR_BASE)
 
 
 typedef struct {
-    double      pos[3], vel[3];
+    double pos[3], vel[3];
+    short oldX, oldY;
+    short newX, newY;
 } Star;
 
 typedef struct {
@@ -78,8 +80,6 @@ typedef struct {
     int         mass;
     int         nstars;
     Star       *stars;
-    XPoint     *oldpoints;
-    XPoint     *newpoints;
     double      pos[3], vel[3];
     int         galcol;
 } Galaxy;
@@ -99,216 +99,211 @@ typedef struct {
     double      rot_x; // rotation of eye around center of universe, around x-axis
 } Universe;
 
-static Universe *universes = NULL;
+class DispMainUi::Universe {
+protected:
+    double      mat[3][3]; // Movement of stars(?)
+    double      scale; // Scale
+    int         midx; // Middle of screen, x
+    int         midy; // Middle of screen, y
+    double      size; //
+    double      diff[3];
+    Galaxy     *galaxies; // the Whole Universe */
+    int         ngalaxies; // # galaxies
+    int         f_hititerations; // # iterations before restart
+    int         step;
+    double      rot_y; // rotation of eye around center of universe, around y-axis
+    double      rot_x; // rotation of eye around center of universe, around x-axis
 
-void FreeGalaxy(ModeInfo * mi)
+    long batchCount;
+};
+
+void DispMainUi::FreeGalaxy(DispTexture &win)
 {
-    Universe  *gp = &universes[MI_SCREEN(mi)];
-    if (gp->galaxies != NULL) {
-        int         i;
+    if (universe_->galaxies != NULL) {
+        int i;
 
         for (i = 0; i < gp->ngalaxies; i++) {
-            Galaxy     *gt = &gp->galaxies[i];
+            Galaxy *gt = &gp->galaxies[i];
 
-            if (gt->stars != NULL)
-                (void) free((void *) gt->stars);
-            if (gt->oldpoints != NULL)
-                (void) free((void *) gt->oldpoints);
-            if (gt->newpoints != NULL)
-                (void) free((void *) gt->newpoints);
+            if (gt->stars != NULL) {
+                delete[] gt->stars;
+            }
         }
-        (void) free((void *) gp->galaxies);
+        delete[] gp->galaxies;
         gp->galaxies = NULL;
     }
 }
 
-void StartOver(ModeInfo * mi)
+void DispMainUi::StartOver(DispTexture &win)
 {
-    Universe  *gp = &universes[MI_SCREEN(mi)];
     int         i, j; /* more tmp */
     double      w1, w2; /* more tmp */
     double      d, v, w, h; /* yet more tmp */
 
-    gp->step = 0;
-    gp->rot_y = 0;
-    gp->rot_x = 0;
+    universe_->step = 0;
+    universe_->rot_y = 0;
+    universe_->rot_x = 0;
 
-    if (MI_BATCHCOUNT(mi) < -MINGALAXIES)
-        FreeGalaxy(mi);
-    gp->ngalaxies = MI_BATCHCOUNT(mi);
-    if (gp->ngalaxies < -MINGALAXIES)
-        gp->ngalaxies = NRAND(-gp->ngalaxies - MINGALAXIES + 1) + MINGALAXIES;
+    if (universe_->batchCount < -MINGALAXIES) {
+        FreeGalaxy(win);
+    }
+    universe_->ngalaxies = universe_->batchCount;
+    if (universe_->ngalaxies < -MINGALAXIES) {
+        universe_->ngalaxies = NRAND(-universe_->ngalaxies - MINGALAXIES + 1) + MINGALAXIES;
+    } else if (universe_->ngalaxies < MINGALAXIES) {
+        universe_->ngalaxies = MINGALAXIES;
+    }
 
-    else if (gp->ngalaxies < MINGALAXIES)
-        gp->ngalaxies = MINGALAXIES;
-    if (gp->galaxies == NULL)
-        gp->galaxies = (Galaxy *) calloc(gp->ngalaxies, sizeof (Galaxy));
+    if (universe_->galaxies == NULL) {
+        universe_->galaxies = new Galaxy[universe_->ngalaxies]();
+    }
 
-    for (i = 0; i < gp->ngalaxies; ++i) {
-        Galaxy     *gt = &gp->galaxies[i];
+    for (i = 0; i < universe_->ngalaxies; ++i) {
+        Galaxy     *gt = &universe_->galaxies[i];
         double      sinw1, sinw2, cosw1, cosw2;
 
-        gt->galcol = NRAND(COLORBASE - 2);
-        if (gt->galcol > 1)
+        gt->galcol = NRAND(COLOR_BASE - 2);
+        if (gt->galcol > 1) {
             gt->galcol += 2; /* Mult 8; 16..31 no green stars */
+        }
         /* Galaxies still may have some green stars but are not all green. */
 
         if (gt->stars != NULL) {
-            (void) free((void *) gt->stars);
+            delete[] gt->stars;
             gt->stars = NULL;
         }
         gt->nstars = (NRAND(MAX_STARS / 2)) + MAX_STARS / 2;
-        gt->stars = (Star *) malloc(gt->nstars * sizeof (Star));
-        gt->oldpoints = (XPoint *) malloc(gt->nstars * sizeof (XPoint));
-        gt->newpoints = (XPoint *) malloc(gt->nstars * sizeof (XPoint));
+        gt->stars = new Star[gt->nstars]();
 
-        w1 = 2.0 * M_PI * FLOATRAND;
-        w2 = 2.0 * M_PI * FLOATRAND;
+        w1 = 2.0 * M_PI * FLOAT_RAND_NUM;
+        w2 = 2.0 * M_PI * FLOAT_RAND_NUM;
         sinw1 = SINF(w1);
         sinw2 = SINF(w2);
         cosw1 = COSF(w1);
         cosw2 = COSF(w2);
 
-        gp->mat[0][0] = cosw2;
-        gp->mat[0][1] = -sinw1 * sinw2;
-        gp->mat[0][2] = cosw1 * sinw2;
-        gp->mat[1][0] = 0.0;
-        gp->mat[1][1] = cosw1;
-        gp->mat[1][2] = sinw1;
-        gp->mat[2][0] = -sinw2;
-        gp->mat[2][1] = -sinw1 * cosw2;
-        gp->mat[2][2] = cosw1 * cosw2;
+        universe_->mat[0][0] = cosw2;
+        universe_->mat[0][1] = -sinw1 * sinw2;
+        universe_->mat[0][2] = cosw1 * sinw2;
+        universe_->mat[1][0] = 0.0;
+        universe_->mat[1][1] = cosw1;
+        universe_->mat[1][2] = sinw1;
+        universe_->mat[2][0] = -sinw2;
+        universe_->mat[2][1] = -sinw1 * cosw2;
+        universe_->mat[2][2] = cosw1 * cosw2;
 
-        gt->vel[0] = FLOATRAND * 2.0 - 1.0;
-        gt->vel[1] = FLOATRAND * 2.0 - 1.0;
-        gt->vel[2] = FLOATRAND * 2.0 - 1.0;
-        gt->pos[0] = -gt->vel[0] * DELTAT * gp->f_hititerations + FLOATRAND -
-                     0.5;
-        gt->pos[1] = -gt->vel[1] * DELTAT * gp->f_hititerations + FLOATRAND -
-                     0.5;
-        gt->pos[2] = -gt->vel[2] * DELTAT * gp->f_hititerations + FLOATRAND -
-                     0.5;
+        gt->vel[0] = FLOAT_RAND_NUM * 2.0 - 1.0;
+        gt->vel[1] = FLOAT_RAND_NUM * 2.0 - 1.0;
+        gt->vel[2] = FLOAT_RAND_NUM * 2.0 - 1.0;
+        gt->pos[0] = -gt->vel[0] * DELTAT * universe_->f_hititerations + FLOAT_RAND_NUM - 0.5;
+        gt->pos[1] = -gt->vel[1] * DELTAT * universe_->f_hititerations + FLOAT_RAND_NUM - 0.5;
+        gt->pos[2] = -gt->vel[2] * DELTAT * universe_->f_hititerations + FLOAT_RAND_NUM - 0.5;
 
-        gt->mass = (int) (FLOATRAND * 1000.0) + 1;
+        gt->mass = (int) (FLOAT_RAND_NUM * 1000.0) + 1;
 
-        gp->size = GALAXYRANGESIZE * FLOATRAND + GALAXYMINSIZE;
+        universe_->size = GALAXYRANGESIZE * FLOAT_RAND_NUM + GALAXYMINSIZE;
 
         for (j = 0; j < gt->nstars; ++j) {
             Star       *st = &gt->stars[j];
-            XPoint     *oldp = &gt->oldpoints[j];
-            XPoint     *newp = &gt->newpoints[j];
-
             double      sinw, cosw;
 
-            w = 2.0 * M_PI * FLOATRAND;
+            w = 2.0 * M_PI * FLOAT_RAND_NUM;
             sinw = SINF(w);
             cosw = COSF(w);
-            d = FLOATRAND * gp->size;
-            h = FLOATRAND * exp(-2.0 * (d / gp->size)) / 5.0 * gp->size;
-            if (FLOATRAND < 0.5)
+            d = FLOAT_RAND_NUM * universe_->size;
+            h = FLOAT_RAND_NUM * exp(-2.0 * (d / universe_->size)) / 5.0 * universe_->size;
+            if (FLOAT_RAND_NUM < 0.5) {
                 h = -h;
-            st->pos[0] = gp->mat[0][0] * d * cosw + gp->mat[1][0] * d * sinw +
-                         gp->mat[2][0] * h + gt->pos[0];
-            st->pos[1] = gp->mat[0][1] * d * cosw + gp->mat[1][1] * d * sinw +
-                         gp->mat[2][1] * h + gt->pos[1];
-            st->pos[2] = gp->mat[0][2] * d * cosw + gp->mat[1][2] * d * sinw +
-                         gp->mat[2][2] * h + gt->pos[2];
+            }
+            st->pos[0] = universe_->mat[0][0] * d * cosw + universe_->mat[1][0] * d * sinw +
+                         universe_->mat[2][0] * h + gt->pos[0];
+            st->pos[1] = universe_->mat[0][1] * d * cosw + universe_->mat[1][1] * d * sinw +
+                         universe_->mat[2][1] * h + gt->pos[1];
+            st->pos[2] = universe_->mat[0][2] * d * cosw + universe_->mat[1][2] * d * sinw +
+                         universe_->mat[2][2] * h + gt->pos[2];
 
             v = sqrt(gt->mass * QCONS / sqrt(d * d + h * h));
-            st->vel[0] = -gp->mat[0][0] * v * sinw + gp->mat[1][0] * v * cosw +
-                         gt->vel[0];
-            st->vel[1] = -gp->mat[0][1] * v * sinw + gp->mat[1][1] * v * cosw +
-                         gt->vel[1];
-            st->vel[2] = -gp->mat[0][2] * v * sinw + gp->mat[1][2] * v * cosw +
-                         gt->vel[2];
-
+            st->vel[0] = -universe_->mat[0][0] * v * sinw + universe_->mat[1][0] * v * cosw + gt->vel[0];
+            st->vel[1] = -universe_->mat[0][1] * v * sinw + universe_->mat[1][1] * v * cosw + gt->vel[1];
+            st->vel[2] = -universe_->mat[0][2] * v * sinw + universe_->mat[1][2] * v * cosw + gt->vel[2];
             st->vel[0] *= DELTAT;
             st->vel[1] *= DELTAT;
             st->vel[2] *= DELTAT;
-
-            oldp->x = 0;
-            oldp->y = 0;
-            newp->x = 0;
-            newp->y = 0;
+            st->oldX = 0;
+            st->oldY = 0;
+            st->newX = 0;
+            st->newY = 0;
         }
-
     }
 
-    XClearWindow(MI_DISPLAY(mi), MI_WINDOW(mi));
+    win.ClearScreen();
 
 #if 0
-    (void) printf("ngalaxies=%d, f_hititerations=%d\n", gp->ngalaxies,
-gp->f_hititerations);
- (void) printf("f_deltat=%g\n", DELTAT);
- (void) printf("Screen: ");
+    (void) printf("ngalaxies=%d, f_hititerations=%d\n", universe_->ngalaxies, universe_->f_hititerations);
+    (void) printf("f_deltat=%g\n", DELTAT);
+    (void) printf("Screen: ");
 #endif /*0 */
 }
 
-void InitGalaxy(ModeInfo * mi)
+void DispMainUi::InitGalaxy(DispTexture &win)
 {
-    Universe  *gp;
+    Universe  *universe_;
 
-    MI_INIT (mi, universes);
-    gp = &universes[MI_SCREEN(mi)];
+    if (universe_ == NULL_PTR) {
+        universe_ = new (DispMainUi::Universe);
+        if (universe_ == NULL_PTR) {
+            return OS_ERROR;
+        }
+    }
 
-# ifdef HAVE_JWXYZ	/* Don't second-guess Quartz's double-buffering */
-    dbufp = False;
-# endif
-
-    gp->f_hititerations = MI_CYCLES(mi);
-
-    gp->scale = (double) (MI_WIN_WIDTH(mi) + MI_WIN_HEIGHT(mi)) / 8.0;
-    gp->midx =  MI_WIN_WIDTH(mi)  / 2;
-    gp->midy =  MI_WIN_HEIGHT(mi) / 2;
-    StartOver(mi);
+    universe_->f_hititerations = 0;
+    universe_->scale = (double) (win.GetWidth() + win.GetHeight()) / 8.0;
+    universe_->midx =  win.GetWidth()  / 2;
+    universe_->midy =  win.GetHeight() / 2;
+    StartOver(win);
 }
 
-void DrawGalaxy(ModeInfo * mi)
+void DrawGalaxy(DispTexture &win)
 {
     Display    *display = MI_DISPLAY(mi);
     Window      window = MI_WINDOW(mi);
     GC          gc = MI_GC(mi);
-    Universe  *gp = &universes[MI_SCREEN(mi)];
     double      d, eps, cox, six, cor, sir;  /* tmp */
     int         i, j, k; /* more tmp */
-    XPoint    *dummy = NULL;
-
-    if (! dbufp)
-        XClearWindow(MI_DISPLAY(mi), MI_WINDOW(mi));
 
     if(spin){
-        gp->rot_y += 0.01;
-        gp->rot_x += 0.004;
+        universe_->rot_y += 0.01;
+        universe_->rot_x += 0.004;
     }
 
-    cox = COSF(gp->rot_y);
-    six = SINF(gp->rot_y);
-    cor = COSF(gp->rot_x);
-    sir = SINF(gp->rot_x);
+    cox = COSF(universe_->rot_y);
+    six = SINF(universe_->rot_y);
+    cor = COSF(universe_->rot_x);
+    sir = SINF(universe_->rot_x);
 
     eps = 1/(EPSILON * sqrt_EPSILON * DELTAT * DELTAT * QCONS);
 
-    for (i = 0; i < gp->ngalaxies; ++i) {
-        Galaxy     *gt = &gp->galaxies[i];
+    for (i = 0; i < universe_->ngalaxies; ++i) {
+        Galaxy     *gt = &universe_->galaxies[i];
 
-        for (j = 0; j < gp->galaxies[i].nstars; ++j) {
+        for (j = 0; j < universe_->galaxies[i].nstars; ++j) {
             Star       *st = &gt->stars[j];
-            XPoint     *newp = &gt->newpoints[j];
             double      v0 = st->vel[0];
             double      v1 = st->vel[1];
             double      v2 = st->vel[2];
 
-            for (k = 0; k < gp->ngalaxies; ++k) {
-                Galaxy     *gtk = &gp->galaxies[k];
+            for (k = 0; k < universe_->ngalaxies; ++k) {
+                Galaxy     *gtk = &universe_->galaxies[k];
                 double      d0 = gtk->pos[0] - st->pos[0];
                 double      d1 = gtk->pos[1] - st->pos[1];
                 double      d2 = gtk->pos[2] - st->pos[2];
 
                 d = d0 * d0 + d1 * d1 + d2 * d2;
-                if (d > EPSILON)
+                if (d > EPSILON) {
                     d = gtk->mass / (d * sqrt(d)) * DELTAT * DELTAT * QCONS;
-                else
+                } else {
                     d = gtk->mass / (eps * sqrt(eps));
+                }
                 v0 += d0 * d;
                 v1 += d1 * d;
                 v2 += d2 * d;
@@ -317,30 +312,27 @@ void DrawGalaxy(ModeInfo * mi)
             st->vel[0] = v0;
             st->vel[1] = v1;
             st->vel[2] = v2;
-
             st->pos[0] += v0;
             st->pos[1] += v1;
             st->pos[2] += v2;
 
-            newp->x = (short) (((cox * st->pos[0]) - (six * st->pos[2])) *
-                               gp->scale) + gp->midx;
-            newp->y = (short) (((cor * st->pos[1]) - (sir * ((six * st->pos[0]) +
-                                                             (cox * st->pos[2]))))
-                               * gp->scale) + gp->midy;
-
+            st->newX = (short) (((cox * st->pos[0]) - (six * st->pos[2])) * universe_->scale) + universe_->midx;
+            st->newY = (short) (((cor * st->pos[1]) - (sir * ((six * st->pos[0]) + (cox * st->pos[2]))))
+                               * universe_->scale) + universe_->midy;
         }
 
-        for (k = i + 1; k < gp->ngalaxies; ++k) {
-            Galaxy     *gtk = &gp->galaxies[k];
+        for (k = i + 1; k < universe_->ngalaxies; ++k) {
+            Galaxy     *gtk = &universe_->galaxies[k];
             double      d0 = gtk->pos[0] - gt->pos[0];
             double      d1 = gtk->pos[1] - gt->pos[1];
             double      d2 = gtk->pos[2] - gt->pos[2];
 
             d = d0 * d0 + d1 * d1 + d2 * d2;
-            if (d > EPSILON)
+            if (d > EPSILON) {
                 d = 1 / (d * sqrt(d)) * DELTAT * QCONS;
-            else
+            } else {
                 d = 1 / (EPSILON * sqrt_EPSILON) * DELTAT * QCONS;
+            }
 
             d0 *= d;
             d1 *= d;
@@ -357,22 +349,30 @@ void DrawGalaxy(ModeInfo * mi)
         gt->pos[1] += gt->vel[1] * DELTAT;
         gt->pos[2] += gt->vel[2] * DELTAT;
 
+        // 如果使用双缓冲，执行下方4行
         if (dbufp) {
             XSetForeground(display, gc, MI_WIN_BLACK_PIXEL(mi));
-            XDrawPoints(display, window, gc, gt->oldpoints, gt->nstars,
-                        CoordModeOrigin);
+            XDrawPoints(display, window, gc, gt->stars, gt->nstars, CoordModeOrigin);
         }
         XSetForeground(display, gc, MI_PIXEL(mi, COLORSTEP * gt->galcol));
-        XDrawPoints(display, window, gc, gt->newpoints, gt->nstars,
-                    CoordModeOrigin);
+        XDrawPoints(display, window, gc, gt->stars, gt->nstars, CoordModeOrigin);
 
-        dummy = gt->oldpoints;
-        gt->oldpoints = gt->newpoints;
-        gt->newpoints = dummy;
+        // 已经显示的新坐标点，存入旧坐标点。双缓冲机制
+        short tmp;
+        for (j = 0; j < universe_->galaxies[i].nstars; ++j) {
+            Star *st = &gt->stars[j];
+            tmp = st->oldX;
+            st->oldX = st->newX;
+            st->newX = tmp;
+            tmp = st->oldY;
+            st->oldY = st->newY;
+            st->newY = tmp;
+        }
     }
 
-    gp->step++;
-    if (gp->step > gp->f_hititerations * 4)
-        StartOver(mi);
+    universe_->step++;
+    if (universe_->step > universe_->f_hititerations * 4) {
+        StartOver(win);
+    }
 }
 
